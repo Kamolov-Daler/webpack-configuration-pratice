fd0b405c48001835d8800b2340a28a42ef8a2b5f
Daler <daler.kamolov.19@mail.ru>
2023-01-12 00:16:06 +0300

    file config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import Post from './Post'
+import './styles/style.css'
+import json from './assets/json'
+
 const post = new Post('Webpack KD');
 
+let jsonstring = 'json';
+
 console.log('post to string', post.toString())
+
+console.log('JSON', json)
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        main: './src/index.js',
-        analytics: './src/analytics.js'
-    },
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new CleanWebpackPlugin()
-    ]
+	context: path.resolve(__dirname, 'src'),
+	mode: 'development',
+	entry: {
+		main: './index.js',
+		analytics: './analytics.js'
+	},
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './index.html'
+		}),
+		new CleanWebpackPlugin()
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	}
 }
